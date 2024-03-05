5e4804ffdd0cc074fb1b264771db55b8aaed41d9
achalkatkar <11achalkatkar@gmail.com>
2024-03-05 19:06:15 +0530

    added files

---
+++ client/src/View/About/About.js
@@ -0,0 +1,15 @@
+import "./About.css";
+
+import React from "react";
+
+function About () {
+    return(
+        <>
+        <div>This is about page </div>
+        
+        </>
+    )
+}
+
+export default About
+
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import About from './View/About/About';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+
+  {
+    'path': "/about",
+    'element': <About/>
+  },
+
+
+])
 root.render(
-  <div>
-    <h1>hello</h1>
-  </div>
+
+   <RouterProvider router={router}/>
+
 );
 
 
